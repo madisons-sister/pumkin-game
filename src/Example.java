c9b9bdd9adc60e22b04442c36ab3a17f86410f8d
Audrey Hill <alh7@g.clemson.edu>
2019-10-19 17:27:21 -0400

    Update Example.java

--- src/Example.java
+++ src/Example.java
@@ -20,8 +20,9 @@ public class Example {
 	static Timer timer;
 
 	public int score = 0;
+	public int val;
 
-	
+
 	private JFrame frame;
 
 	/**
@@ -56,17 +57,17 @@ public class Example {
 		frame.setBounds(100, 100, 735, 495);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel scoreVar = new JLabel("");
 		scoreVar.setForeground(Color.WHITE);
 		scoreVar.setBounds(74, 13, 69, 20);
 		frame.getContentPane().add(scoreVar);
-		
+
 		JLabel lblScore = new JLabel("SCORE: ");
 		lblScore.setForeground(Color.WHITE);
 		lblScore.setBounds(10, 11, 66, 24);
 		frame.getContentPane().add(lblScore);
-		
+
 		JButton btnStart = new JButton("START");
 		btnStart.setFont(new Font("Tahoma", Font.PLAIN, 26));
 		btnStart.addMouseListener(new MouseAdapter() {
@@ -79,7 +80,6 @@ public class Example {
 		btnStart.setForeground(Color.RED);
 		btnStart.setBounds(295, 11, 155, 41);
 		frame.getContentPane().add(btnStart);
-		
 
 
 		JLabel timerLabel = new JLabel("");
@@ -88,389 +88,333 @@ public class Example {
 		timerLabel.setBounds(295, 76, 155, 41);
 		frame.getContentPane().add(timerLabel);
 
-		boolean clicked = false;
-		
 		JLabel label_2 = new JLabel("");
 		label_2.setIcon(new ImageIcon("pumkin.png"));
 		label_2.setBounds(138, 391, 60, 54);
 		frame.getContentPane().add(label_2);
-		
+
 		label_2.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
 
-				if(!timerLabel.getText().equals("Time's up!!")) {
-				score++;
-
-				if (clicked == false) {
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
 					score += 100;
 					scoreVar.setText(Integer.toString(score));
 					label_2.setIcon(new ImageIcon("dead pumkin.png"));
-				} 
-
-				
-				boolean clicked = true;
 
 
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
 				}
-				
+
 			}
 		});
-		
+
 		JLabel label_3 = new JLabel("");
 		label_3.setIcon(new ImageIcon("pumkin.png"));
 		label_3.setBounds(202, 391, 60, 54);
 		frame.getContentPane().add(label_3);
-		
+
 		label_3.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
 
-				if(!timerLabel.getText().equals("Time's up!!")) {
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
+					score+=100;
 
-				score++;
+					scoreVar.setText(Integer.toString(score));
+					label_3.setIcon(new ImageIcon("dead pumkin.png"));
 
-				score+=100;
-
-				scoreVar.setText(Integer.toString(score));
-				label_3.setIcon(new ImageIcon("dead pumkin.png"));
-
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
 				}
 			}
 		});
-		
+
 		JLabel label_5 = new JLabel("");
 		label_5.setIcon(new ImageIcon("pumkin.png"));
 		label_5.setBounds(331, 391, 60, 54);
 		frame.getContentPane().add(label_5);
-		
+
 		label_5.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
+					//SICK PUMPKIN
 
-				if(!timerLabel.getText().equals("Time's up!!")) {
-				score++;
 
-				score-=50;
-
-				
-				scoreVar.setText(Integer.toString(score));
-				label_5.setIcon(new ImageIcon("sick pumkin.png"));
-
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
-			}
+					scoreVar.setText(Integer.toString(score));
+					label_5.setIcon(new ImageIcon("sick pumkin.png"));
+					timerLabel.setText("Game Over!");
+					val = -1;
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
+				}
 			}
 		});
-		
+
 		JLabel label_6 = new JLabel("");
 		label_6.setIcon(new ImageIcon("pumkin.png"));
 		label_6.setBounds(10, 326, 60, 54);
 		frame.getContentPane().add(label_6);
-		
+
 		label_6.addMouseListener(new MouseAdapter() {
-	
+
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
 
-				
-				if(!timerLabel.getText().equals("Time's up!!")) {
 
-				score++;
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
+					score+=100;
 
-				score+=100;
+					scoreVar.setText(Integer.toString(score));
+					label_6.setIcon(new ImageIcon("dead pumkin.png"));
 
-				scoreVar.setText(Integer.toString(score));
-				label_6.setIcon(new ImageIcon("dead pumkin.png"));
-
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
-			}
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
+				}
 			}
 		});
-		
+
 		JLabel label_7 = new JLabel("");
 		label_7.setIcon(new ImageIcon("pumkin.png"));
 		label_7.setBounds(652, 391, 60, 54);
 		frame.getContentPane().add(label_7);
-		
+
 		label_7.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
 
-				if(!timerLabel.getText().equals("Time's up!!")) {
+					//SICK PUMPKIN
 
-				score++;
-
-				score-=50;
-
-				
-				scoreVar.setText(Integer.toString(score));
-				label_7.setIcon(new ImageIcon("sick pumkin.png"));
-
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
-			}
+					scoreVar.setText(Integer.toString(score));
+					label_7.setIcon(new ImageIcon("sick pumkin.png"));
+					timerLabel.setText("Game Over!");
+					val = -1;
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
+				}
 			}
 		});
-		
+
 		JLabel label_8 = new JLabel("");
 		label_8.setIcon(new ImageIcon("pumkin.png"));
 		label_8.setBounds(588, 391, 60, 54);
 		frame.getContentPane().add(label_8);
-		
+
 		label_8.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
 
-				if(!timerLabel.getText().equals("Time's up!!")) {
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
+					score+=100;
 
-				score++;
+					scoreVar.setText(Integer.toString(score));
+					label_8.setIcon(new ImageIcon("dead pumkin.png"));
 
-				score+=100;
-
-
-				
-				scoreVar.setText(Integer.toString(score));
-				label_8.setIcon(new ImageIcon("dead pumkin.png"));
-
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
-			}
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
+				}
 			}
 		});
-		
+
 		JLabel label_9 = new JLabel("");
 		label_9.setIcon(new ImageIcon("pumkin.png"));
 		label_9.setBounds(524, 391, 60, 54);
 		frame.getContentPane().add(label_9);
-		
+
 		label_9.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
+					score+=100;
 
-				
-				if(!timerLabel.getText().equals("Time's up!!")) {
+					scoreVar.setText(Integer.toString(score));
+					label_9.setIcon(new ImageIcon("dead pumkin.png"));
 
-				score++;
-
-				score+=100;
-
-				
-				scoreVar.setText(Integer.toString(score));
-				label_9.setIcon(new ImageIcon("dead pumkin.png"));
-
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
-			}
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
+				}
 			}
 		});
-		
+
 		JLabel label_11 = new JLabel("");
 		label_11.setIcon(new ImageIcon("pumkin.png"));
 		label_11.setBounds(396, 391, 60, 54);
 		frame.getContentPane().add(label_11);
-		
+
 		label_11.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
 
-				
-				if(!timerLabel.getText().equals("Time's up!!")) {
 
-				score++;
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
+					score+=100;
 
-				score+=100;
+					scoreVar.setText(Integer.toString(score));
+					label_11.setIcon(new ImageIcon("dead pumkin.png"));
 
-				
-				scoreVar.setText(Integer.toString(score));
-				label_11.setIcon(new ImageIcon("dead pumkin.png"));
-
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
-			}
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
+				}
 			}
 		});
-		
+
 		JLabel label_12 = new JLabel("");
 		label_12.setIcon(new ImageIcon("pumkin.png"));
 		label_12.setBounds(74, 326, 60, 54);
 		frame.getContentPane().add(label_12);
-		
+
 		label_12.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
+					//SICK PUMPKIN
 
-				if(!timerLabel.getText().equals("Time's up!!")) {
+					scoreVar.setText(Integer.toString(score));
+					label_12.setIcon(new ImageIcon("sick pumkin.png"));
+					timerLabel.setText("Game Over!");
+					val = -1;
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
+				}
 
-				score++;
-			
-				score-=50;
-				
-
-				scoreVar.setText(Integer.toString(score));
-				label_12.setIcon(new ImageIcon("sick pumkin.png"));
-
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
-			}
 			}
 		});
-		
+
 		JLabel label_15 = new JLabel("");
 		label_15.setIcon(new ImageIcon("pumkin.png"));
 		label_15.setBounds(267, 326, 60, 54);
 		frame.getContentPane().add(label_15);
-		
+
 		label_15.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
 
-				if(!timerLabel.getText().equals("Time's up!!")) {
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
 
-				score++;
+					score+=100;
 
-				score+=100;
-				
-				scoreVar.setText(Integer.toString(score));
-				label_15.setIcon(new ImageIcon("dead pumkin.png"));
+					scoreVar.setText(Integer.toString(score));
+					label_15.setIcon(new ImageIcon("dead pumkin.png"));
 
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
-			}
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
+				}
 			}
 		});
-		
+
 		JLabel label_17 = new JLabel("");
 		label_17.setIcon(new ImageIcon("pumkin.png"));
 		label_17.setBounds(396, 326, 60, 54);
 		frame.getContentPane().add(label_17);
-		
+
 		label_17.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
 
-				if(!timerLabel.getText().equals("Time's up!!")) {
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
 
-				score++;
+					score+=100;
 
-				score+=100;
+					scoreVar.setText(Integer.toString(score));
+					label_17.setIcon(new ImageIcon("dead pumkin.png"));
 
-
-				
-				scoreVar.setText(Integer.toString(score));
-				label_17.setIcon(new ImageIcon("dead pumkin.png"));
-
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
-			}
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
+				}
 			}
 		});
-		
+
 		JLabel label_20 = new JLabel("");
 		label_20.setIcon(new ImageIcon("pumkin.png"));
 		label_20.setBounds(10, 391, 60, 54);
 		frame.getContentPane().add(label_20);
-		
+
 		label_20.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				
-				if(!timerLabel.getText().equals("Time's up!!")) {
 
-				score++;
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
 
-				score-=50;
-				
-				scoreVar.setText(Integer.toString(score));				
-				label_20.setIcon(new ImageIcon("sick pumkin.png"));
+					//SICK PUMPKIN
+					scoreVar.setText(Integer.toString(score));				
+					label_20.setIcon(new ImageIcon("sick pumkin.png"));
+					timerLabel.setText("Game Over!");
+					val = -1;
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
 
-				
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
-			}
+				}
 			}
 		});
-		
+
 		JLabel label_22 = new JLabel("");
 		label_22.setIcon(new ImageIcon("pumkin.png"));
 		label_22.setBounds(10, 261, 60, 54);
 		frame.getContentPane().add(label_22);
-		
+
 		label_22.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
 
-				if(!timerLabel.getText().equals("Time's up!!")) {
-
-				score++;
-
-
-				if (clicked == false) {
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
 					score+= 100;
-					
+
 					scoreVar.setText(Integer.toString(score));
 					label_22.setIcon(new ImageIcon("dead pumkin.png"));
-//					boolean clicked = true;
-				}
 
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
+
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
 				}
 			}
 		});
-		
+
 		JLabel label_24 = new JLabel("");
 		label_24.setIcon(new ImageIcon("pumkin.png"));
 		label_24.setBounds(138, 261, 60, 54);
 		frame.getContentPane().add(label_24);
-		
+
 		label_24.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 
 
-				if(!timerLabel.getText().equals("Time's up!!")) {
+				if(!timerLabel.getText().equals("Time's up!!") && !timerLabel.getText().equals("Game Over!")) {
+					System.out.println(timerLabel.getText());
+					score+=100;
 
-				score++;
+					scoreVar.setText(Integer.toString(score));
+					label_24.setIcon(new ImageIcon("dead pumkin.png"));
 
-				score+=100;
-
-
-				
-				scoreVar.setText(Integer.toString(score));
-				label_24.setIcon(new ImageIcon("dead pumkin.png"));
-
-				frame.getContentPane().revalidate();
-				frame.getContentPane().repaint();
-			}
+					frame.getContentPane().revalidate();
+					frame.getContentPane().repaint();
+				}
 			}
 		});
-		
+
 		JLabel label = new JLabel("");
 		label.setIcon(new ImageIcon("spooky_tree.jpg"));
 		label.setBounds(466, 76, 240, 304);
 		frame.getContentPane().add(label);
-		
-		
+
+
 		try {
-			Timer timer= new Timer();
+			timer= new Timer();
 			int delay = 1000;
 			int period = 1000;
 			String secs= "10";
@@ -479,11 +423,13 @@ public class Example {
 			timer.scheduleAtFixedRate(new TimerTask() {
 				public void run() {
 
-					int val= setInterval();
+					val= setInterval();
 
-					if(val>0) {	
+					if(timerLabel.getText().equals("Game Over!")) {	
+						//timerLabel.setText("Game Over!");
+					}else if(val>0){
 						timerLabel.setText("Time Left: " + val + " sec");
-					}else {
+					} else {
 						timerLabel.setText("Time's up!!");
 					}
 
@@ -492,15 +438,15 @@ public class Example {
 		} catch (NullPointerException e) {
 			//e.printStackTrace();
 		}
-		    
+
 	}
 	private static final int setInterval() {
-	    if (interval != 0) {
-	        return --interval;
-	    }else {
-	    	return -1;
-	    }
-	    
+		if (interval != 0) {
+			return --interval;
+		}else {
+			return -1;
+		}
+
 	}
 }
 
